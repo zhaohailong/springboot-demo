aca18f40e2b9092767d816932f38300f9c075b01
zhaohailong <zhaohailong@jushiwangedu.com>
2020-08-09 13:46:04 +0800

    init

---
+++ src/main/java/com/jingtongkeji/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.jingtongkeji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@GetMapping("/")
+	public String hello() {
+		return "hello world";
+	}
+
+
+}
---
+++ src/test/java/com/jingtongkeji/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jingtongkeji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
